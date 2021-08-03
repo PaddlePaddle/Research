1f6a311a3a76d4cb237aab49bbfb4201de0fba43
Ruiyang Ren <46014149+ReyonRen@users.noreply.github.com>
2021-08-03 16:40:48 +0800

    Update wget_trained_model.sh

--- NLP/ACL2021-PAIR/wget_trained_model.sh
+++ NLP/ACL2021-PAIR/wget_trained_model.sh
@@ -1,4 +1,5 @@
 wget -nv --no-check-certificate https://rocketqa.bj.bcebos.com/PAIR/models.tar.gz
+tar -zxf models.tar.gz
 cd models
 
 for filename in ernie_base_twin_init.tar.gz marco_finetuned_encoder.tar.gz nq_finetuned_encoder.tar.gz;do
