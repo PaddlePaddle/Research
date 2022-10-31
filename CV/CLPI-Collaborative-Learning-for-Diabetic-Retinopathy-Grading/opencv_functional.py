0c26c38578be56e1006ab7be5889fe9728b75f8f
parap1uie-s <parap1uie-s@users.noreply.github.com>
2022-10-31 10:26:03 +0800

    Update opencv_functional.py

--- CV/CLPI-Collaborative-Learning-for-Diabetic-Retinopathy-Grading/opencv_functional.py
+++ CV/CLPI-Collaborative-Learning-for-Diabetic-Retinopathy-Grading/opencv_functional.py
@@ -300,51 +300,6 @@ def adjust_brightness(img, brightness_factor):
         return cv2.LUT(img, table)
 
 
-def adjust_hue(img, hue_factor):
-    """Adjust hue of an image.
-    The image hue is adjusted by converting the image to HSV and
-    cyclically shifting the intensities in the hue channel (H).
-    The image is then converted back to original image mode.
-    `hue_factor` is the amount of shift in H channel and must be in the
-    interval `[-0.5, 0.5]`.
-    See `Hue`_ for more details.
-    .. _Hue: https://en.wikipedia.org/wiki/Hue
-    Args:
-        img (numpy ndarray): numpy ndarray to be adjusted.
-        hue_factor (float):  How much to shift the hue channel. Should be in
-            [-0.5, 0.5]. 0.5 and -0.5 give complete reversal of hue channel in
-            HSV space in positive and negative direction respectively.
-            0 means no shift. Therefore, both -0.5 and 0.5 will give an image
-            with complementary colors while 0 gives the original image.
-    Returns:
-        numpy ndarray: Hue adjusted image.
-    """
-    # After testing, found that OpenCV calculates the Hue in a call to
-    # cv2.cvtColor(..., cv2.COLOR_BGR2HSV) differently from PIL
-
-    # This function takes 160ms! should be avoided
-    if not(-0.5 <= hue_factor <= 0.5):
-        raise ValueError(
-            'hue_factor is not in [-0.5, 0.5]. Got {}'.format(hue_factor))
-    if not _is_numpy_image(img):
-        raise TypeError('img should be numpy Image. Got {}'.format(type(img)))
-    img = Image.fromarray(img)
-    input_mode = img.mode
-    if input_mode in {'L', '1', 'I', 'F'}:
-        return np.array(img)
-
-    h, s, v = img.convert('HSV').split()
-
-    np_h = np.array(h, dtype=np.uint8)
-    # uint8 addition take cares of rotation across boundaries
-    with np.errstate(over='ignore'):
-        np_h += np.uint8(hue_factor * 255)
-    h = Image.fromarray(np_h, 'L')
-
-    img = Image.merge('HSV', (h, s, v)).convert(input_mode)
-    return np.array(img)
-
-
 def adjust_gamma(img, gamma, gain=1):
     r"""Perform gamma correction on an image.
     Also known as Power Law Transform. Intensities in RGB mode are adjusted
