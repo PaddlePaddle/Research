bd8e584778dc3dfe77319754411ac053b760cbed
rudaoshi <sunmingming@gmail.com>
2022-11-26 11:18:57 +0800

    remove unnecessary file

--- EE/paddlemetric/src/build/lib/paddlemetrics/utilities/__init__.py
+++
@@ -1,3 +0,0 @@
-from paddlemetrics.utilities.data import apply_to_collection  # noqa: F401
-from paddlemetrics.utilities.distributed import class_reduce, reduce  # noqa: F401
-from paddlemetrics.utilities.prints import rank_zero_debug, rank_zero_info, rank_zero_warn  # noqa: F401
